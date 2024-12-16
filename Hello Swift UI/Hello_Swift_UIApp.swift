913b27742026b754f6a17ec6f79b56d6a049ce0d
aliozanoguz <ali.oguz@hisarschool.k12.tr>
2024-12-16 08:10:15 +0300

    Initial Commit

---
+++ Hello Swift UI/Hello_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_Swift_UIApp.swift
+//  Hello Swift UI
+//
+//  Created by Ali Oguz on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Hello_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hello Swift UITests/Hello_Swift_UITests.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_Swift_UITests.swift
+//  Hello Swift UITests
+//
+//  Created by Ali Oguz on 28.10.2024.
+//
+
+import Testing
+@testable import Hello_Swift_UI
+
+struct Hello_Swift_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
